88ff03c2ad24f1ea3604521f1131bb08c2a089c2
Nitin <nsinghboss02@gmail.com>
2022-01-23 16:05:17 +0530

    Write a program to print numbers from 1 to n in increasing order recursively.

--- src/com/Recursion/Question2.java
+++ src/com/Recursion/Question2.java
@@ -4,11 +4,22 @@ import java.util.*;
 public class Question2 {
     public static void main(String[] args) {
     /*
-
+     Write a program to print numbers from 1 to n in increasing order recursively.
+     Sample Input 1 :
+     6
+     Sample Output 1 :
+     1 2 3 4 5 6
      */
-
+      int n = 6;
+      print(n);
 
 
     }
-
+    public static void print(int n){
+        if(n==0){
+            return;
+        }
+        print(n-1);
+        System.out.println(n);
+    }
 }
